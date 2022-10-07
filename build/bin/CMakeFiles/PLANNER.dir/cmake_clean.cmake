3bab112caecf97418face01ed0de266f89f36ca1
gao chengxi <1825752554@qq.com>
2022-10-07 10:34:47 +0800

    modify src/CMakeLists.txt

---
+++ build/bin/CMakeFiles/PLANNER.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PLANNER.dir/main.cpp.o"
+  "PLANNER.pdb"
+  "PLANNER"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PLANNER.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
