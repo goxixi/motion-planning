37ae6d17bc6bb370f2242f09a71fb41f23443a84
chengxi gao <1825752554@qq.com>
2022-11-09 21:37:54 +0800

    add d_star.h

---
+++ src/graph_search/d_star.h
@@ -0,0 +1,19 @@
+#ifndef _D_STAR_H_
+#define _D_STAR_H_
+
+#include "graph_search.h"
+
+class DStar : public GraphSearcher {
+private:
+    /* data */
+public:
+    DStar(/* args */);
+    ~DStar();
+};
+
+DStar::DStar(/* args */) {}
+
+DStar::~DStar() {}
+
+
+#endif // !_D_STAR_H_
