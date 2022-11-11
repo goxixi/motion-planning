663ec167efa24e3e62c30b01b3b51b22769ce814
chengxi gao <1825752554@qq.com>
2022-11-11 12:23:53 +0800

    modify

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,22 @@
 #include "RRT/RRT.h"
 #include "graph_search/dijkstra.h"
 #include "graph_search/a_star.h"
-#include "mathematics/obvp_solver.hpp"
 
 int main(void){
     string picture = "../map/map1.png";
-    Map map(picture, 100, 20, 500, 600); // map.png
+    GripMap map(picture, 100, 20, 500, 600); // map.png
         
-    // Map map(picture, 400, 400, 400, 50); // map3.png
+    // GripMap map(picture, 400, 400, 400, 50); // map3.png
     // RRT rrt(map);
     // rrt.findPath();
 
-    // Dijkstra dijkstra;
-    // dijkstra.setMap(map);
-    // dijkstra.findPath();
+    Dijkstra dijkstra;
+    dijkstra.setMap(map);
+    dijkstra.findPath();
 
-    AStar astar;
-    astar.setMap(map);
-    astar.findPath();
+    // AStar astar;
+    // astar.setMap(map);
+    // astar.findPath();
 
 
     return 0;
